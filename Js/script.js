b8382d4e91be049ce302923fc9e9fdb635adae0d
ali <mechanical.engineer.babaie@gmail.com>
2025-04-10 15:07:31 +0430

    section6 session49

--- Js/script.js
+++ Js/script.js
@@ -1,2 +1,26 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+//  Basic &  Stacking & Placement
+
+ const toastElList = document.querySelectorAll('.toast')
+ const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, {
+     animation: true ,
+     autohide: false ,
+     delay: 3000   
+ } ))
+
+ toastList.forEach(
+     function (toast){
+         toast.show();
+     }
+ );
+
+// Live example
+
+ document.getElementById('liveToastBtn').onmouseover = function () {
+     var toastEl = document.querySelector('#liveToast');
+     var myToast = new bootstrap.Toast(toastEl, {
+          animation: true ,
+          autohide: false ,
+             delay: 3000   
+      });
+      myToast.show(); 
+ }
