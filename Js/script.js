e2dc30e91181684ce0a430c06820e9c20116ed66
ali <mechanical.engineer.babaie@gmail.com>
2025-04-12 18:53:36 +0430

    section6 session50

--- Js/script.js
+++ Js/script.js
@@ -1,26 +1,3 @@
-//  Basic &  Stacking & Placement
-
- const toastElList = document.querySelectorAll('.toast')
- const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, {
-     animation: true ,
-     autohide: false ,
-     delay: 3000   
- } ))
-
- toastList.forEach(
-     function (toast){
-         toast.show();
-     }
- );
-
-// Live example
-
- document.getElementById('liveToastBtn').onmouseover = function () {
-     var toastEl = document.querySelector('#liveToast');
-     var myToast = new bootstrap.Toast(toastEl, {
-          animation: true ,
-          autohide: false ,
-             delay: 3000   
-      });
-      myToast.show(); 
- }
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(
+    tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
