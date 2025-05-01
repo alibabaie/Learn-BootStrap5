2e49d230ca0a0dde3e517b5c24fc882d8ebdb787
ali <mechanical.engineer.babaie@gmail.com>
2025-05-01 17:44:08 +0430

    section10 session76

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,24 @@
 $ (function(){
 setTimeout(LoadingEnd,1000);
+
+// start dropdown
+
+$('.custom-dropdown').on('show.bs.dropdown', function() {
+    // $(this).find('.dropdown-menu').first().stop(false, false).slideDown();
+     // $(this).find('.dropdown-menu').addClass('active');
+     var that = $(this);
+    setTimeout(function(){
+        that.find('.dropdown-menu').addClass('active');
+    }, 100);
+    
+
+  });
+  $('.custom-dropdown').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').removeClass('active');
+  });
+
+// end dropdown
+
 })
 
 function LoadingEnd () {
